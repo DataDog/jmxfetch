fadec438d6e1c5988cd95051e19d05554bd336d5
Remi Hakim <remi@datadoghq.com>
2013-12-11 18:49:39 -0500

    Generate more reliable key in connection Manager

--- src/main/java/org/datadog/jmxfetch/ConnectionManager.java
+++ src/main/java/org/datadog/jmxfetch/ConnectionManager.java
@@ -12,7 +12,7 @@ import org.apache.log4j.Logger;
  *
  */
 public class ConnectionManager {
-	private HashMap<LinkedHashMap<String, Object>, Connection> _connections;
+	private HashMap<String, Connection> _connections;
 	private static ConnectionManager _instance = null;
 	private final static Logger LOGGER = Logger.getLogger(ConnectionManager.class.getName());
 	
@@ -24,20 +24,29 @@ public class ConnectionManager {
 	}
 	
 	private ConnectionManager() {
-		_connections = new HashMap<LinkedHashMap<String, Object>, Connection>();
+		_connections = new HashMap<String, Connection>();
 	}
 	
 	 
 	
 	public Connection getConnection(LinkedHashMap<String, Object> connectionParams, boolean forceNewConnection) throws IOException {
-		if (this._connections.containsKey(connectionParams) && !forceNewConnection) {
-			return this._connections.get(connectionParams);
+		String key = generateKey(connectionParams);
+		if (this._connections.containsKey(key) && !forceNewConnection) {
+			return this._connections.get(key);
 		}
-		LOGGER.info("Connection not existing. Creating new connection");
+		if ( forceNewConnection ){
+			LOGGER.info("Forcing the creation of a new connection");
+		} else {
+			LOGGER.info("Connection not existing. Creating new connection");
+		}
 		Connection jmxConnection = new Connection(connectionParams);
-		this._connections.put(connectionParams, jmxConnection);
+		this._connections.put(key, jmxConnection);
 		return jmxConnection;
 				
+	}
+	
+	private static String generateKey(LinkedHashMap<String, Object> connectionParams) {
+		return connectionParams.get("host") + ":" + connectionParams.get("port") + ":" + connectionParams.get("user");
 	}
 	
 }
