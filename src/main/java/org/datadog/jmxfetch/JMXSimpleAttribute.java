c462a0dfafc281306504d1fe463e54d9af9830a5
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2015-10-20 17:40:26 -0400

    Refactoring for code quality.

--- src/main/java/org/datadog/jmxfetch/JMXSimpleAttribute.java
+++ src/main/java/org/datadog/jmxfetch/JMXSimpleAttribute.java
@@ -89,12 +89,12 @@ public class JMXSimpleAttribute extends JMXAttribute {
         } else if (include.getAttribute() instanceof LinkedHashMap<?, ?>) {
             LinkedHashMap<String, LinkedHashMap<String, String>> attribute = (LinkedHashMap<String, LinkedHashMap<String, String>>) (include.getAttribute());
             alias = attribute.get(getAttribute().getName()).get("alias");
-            if (alias == null) {
-                alias = "jmx." + getBeanStringName().split(":")[0] + "." + getAttributeName();
-            }
         } else if (conf.get("metric_prefix") != null) {
             alias = conf.get("metric_prefix") + "." + getBeanStringName().split(":")[0] + "." + getAttributeName();
-        } else {
+        }
+
+        //If still null - generate generic alias,
+        if (alias == null) {
             alias = "jmx." + getBeanStringName().split(":")[0] + "." + getAttributeName();
         }
         alias = convertMetricName(alias);
