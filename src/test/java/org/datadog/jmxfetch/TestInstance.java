0f59408aff28531715642d53e6f7673923c1b861
Greg Meyer <greg.meyer@datadoghq.com>
2017-04-27 12:36:52 -0400

    adding a test

---
+++ src/test/java/org/datadog/jmxfetch/TestInstance.java
@@ -0,0 +1,33 @@
+package org.datadog.jmxfetch;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+public class TestInstance extends TestCommon {
+	
+	@Test
+	public void testMinCollectionInterval() throws Exception {
+		registerMBean(new SimpleTestJavaApp(), "org.datadog.jmxfetch.test:foo=Bar,qux=Baz");
+		initApplication("jmx_min_collection_period.yml");
+		
+		run();
+		
+		LinkedList<HashMap<String, Object>> metrics = getMetrics();
+		assertEquals(15, metrics.size());
+		run();
+		metrics = getMetrics();
+		assertEquals(0, metrics.size());
+		Thread.sleep(300000);
+		
+		run();
+		
+		metrics = getMetrics();
+		assertEquals(15, metrics.size());
+		
+	}
+
+}
