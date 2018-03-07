25b7061efdd22ff5d940c450319effe0366ab8be
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-03-07 13:15:22 -0500

    Uses its own yaml parameter, defaults back 15 seconds

--- src/main/java/org/datadog/jmxfetch/RemoteConnection.java
+++ src/main/java/org/datadog/jmxfetch/RemoteConnection.java
@@ -21,7 +21,7 @@ public class RemoteConnection extends Connection {
     private String rmi_timeout;
     private static final String TRUST_STORE_PATH_KEY = "trust_store_path";
     private static final String TRUST_STORE_PASSWORD_KEY = "trust_store_password";
-    private static final String DEFAULT_RMI_RESPONSE_TIMEOUT = "600000"; //Match the refresh_beans default
+    private static final String DEFAULT_RMI_RESPONSE_TIMEOUT = "15000"; //Match the collection period default
     private final static Logger LOGGER = Logger.getLogger(Connection.class.getName());
 
     public RemoteConnection(LinkedHashMap<String, Object> connectionParams)
@@ -35,10 +35,10 @@ public class RemoteConnection extends Connection {
         user = (String) connectionParams.get("user");
         password = (String) connectionParams.get("password");
         jmx_url = (String) connectionParams.get("jmx_url");
-        	rmi_timeout = (String) connectionParams.get("refresh_beans");
-        	if (rmi_timeout == null) {
-        		rmi_timeout = DEFAULT_RMI_RESPONSE_TIMEOUT;
-        	}
+        rmi_timeout = (String) connectionParams.get("rmi_client_timeout");
+        if (rmi_timeout == null) {
+            rmi_timeout = DEFAULT_RMI_RESPONSE_TIMEOUT;
+        }
         if (connectionParams.containsKey("path")){
             path = (String) connectionParams.get("path");
         }
