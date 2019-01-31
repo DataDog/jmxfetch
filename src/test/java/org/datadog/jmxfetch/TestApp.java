0069a0609c838a4fe1b6a46f96a3459236cefb66
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2019-01-31 08:28:28 +0100

    fixing rebase mishap

--- src/test/java/org/datadog/jmxfetch/TestApp.java
+++ src/test/java/org/datadog/jmxfetch/TestApp.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
