a5b7570f38918e24b76f3fe4107cf574f79acb7a
HantingZHANG <hanting.zhang@datadoghq.com>
2020-06-02 16:03:35 +0200

    check style

--- src/main/java/org/datadog/jmxfetch/util/CustomLogger.java
+++ src/main/java/org/datadog/jmxfetch/util/CustomLogger.java
@@ -21,7 +21,8 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 @Slf4j
 public class CustomLogger {
     private static final Multiset<String> messageCount = HashMultiset.create();
-    private static final String LOGGER_LAYOUT = "%d{yyyy-MM-dd HH:mm:ss z} | JMX | %-5p | %c{1} | %m%n";
+    private static final String layout = "%d{yyyy-MM-dd HH:mm:ss z} | JMX | %-5p | %c{1} | %m%n";
+    private static final String LOGGER_LAYOUT = layout;
     // log4j2 uses SYSTEM_OUT and SYSTEM_ERR - support both
     private static final String SYSTEM_OUT_ALT = "STDOUT";
     private static final String SYSTEM_ERR_ALT = "STDERR";
