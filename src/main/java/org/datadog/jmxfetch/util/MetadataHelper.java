a2462a10a506b3f849e11e0b3a63ab27cfc90459
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2020-11-11 00:43:42 +0100

    [MetadataHelper] Close stream opened to read app properties (#338)
    * [MetadataHelper] Close stream opened to read app properties
    
    In order to fix a memory leak
    
    * address review

--- src/main/java/org/datadog/jmxfetch/util/MetadataHelper.java
+++ src/main/java/org/datadog/jmxfetch/util/MetadataHelper.java
@@ -1,15 +1,16 @@
 package org.datadog.jmxfetch.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class MetadataHelper {
     /**  Returns our own version number. */
     public static String getVersion() {
-        try {
-            final Properties properties = new Properties();
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            properties.load(classLoader.getResourceAsStream("project.properties"));
+        final Properties properties = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try (InputStream stream = classLoader.getResourceAsStream("project.properties")) {
+            properties.load(stream);
             return properties.getProperty("version");
         } catch (IOException e) {
             e.printStackTrace();
