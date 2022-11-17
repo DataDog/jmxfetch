7ba86cfcf6be90f0a76c5d26ed5bede9ba862af7
Carlos Roman <carlos.roman@datadoghq.com>
2022-11-17 17:44:22 +0000

    feat: Test server can now create multiple MBeans

--- tools/test-server/src/main/java/com/datadoghq/jmxfetch/test/Service.java
+++ tools/test-server/src/main/java/com/datadoghq/jmxfetch/test/Service.java
@@ -8,14 +8,22 @@ import java.util.TimerTask;
 
 public class Service {
     public static void main(String[] args) throws Exception {
+        long totalMetrics = 1;
+        if (args.length == 1) {
+            totalMetrics = Long.parseLong(args[0]);
+        }
         final MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        final MetricsMBean metrics = new Metrics("Example");
-        mbs.registerMBean(metrics, null);
+        final MetricsDAO metricsDAO = new MetricsDAO();
+        for (int i = 0; i < totalMetrics; i++) {
+            final String metricName = String.format("Example_%010d", i);
+            final MetricsMBean metrics = new Metrics(metricName, metricsDAO);
+            mbs.registerMBean(metrics, null);
+        }
         final TimerTask task = new TimerTask() {
             public void run() {
                 System.out.println("Task performed on: " + new Date() + ", " +
                         "Thread's name: " + Thread.currentThread().getName());
-                metrics.Do();
+                metricsDAO.Do();
             }
         };
         final Timer timer = new Timer("Timer");
