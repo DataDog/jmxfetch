4a7a076959256db3be0eb43a857d7bb58bb046ae
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2023-04-14 15:09:33 +0000

    Remove redundant API call
    getMBeanInfo is the slowest part of the attribute matching
    process. Speed up the process by making the call once and reusing the
    info object to get everything we need.

--- src/main/java/org/datadog/jmxfetch/Connection.java
+++ src/main/java/org/datadog/jmxfetch/Connection.java
@@ -18,6 +18,7 @@ import javax.management.Attribute;
 import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
 import javax.management.IntrospectionException;
+import javax.management.MBeanInfo;
 import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanException;
 import javax.management.MBeanServerConnection;
@@ -36,17 +37,10 @@ public class Connection {
     protected JMXServiceURL address;
 
     /** Gets attributes for matching bean name. */
-    public MBeanAttributeInfo[] getAttributesForBean(ObjectName beanName)
+    public MBeanInfo getMBeanInfo(ObjectName beanName)
             throws InstanceNotFoundException, IntrospectionException, ReflectionException,
                     IOException {
-        return mbs.getMBeanInfo(beanName).getAttributes();
-    }
-
-    /** Gets class name for matching bean name. */
-    public String getClassNameForBean(ObjectName beanName)
-            throws InstanceNotFoundException, IntrospectionException, ReflectionException,
-            IOException {
-        return mbs.getMBeanInfo(beanName).getClassName();
+        return mbs.getMBeanInfo(beanName);
     }
 
     /** Queries beans on specific scope. Returns set of matching query names.. */
