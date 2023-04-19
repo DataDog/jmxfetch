648a18e702cfbe31b1ab53eec5aab329c6e64933
Scott Opell <me@scottopell.com>
2023-04-19 14:56:24 +0000

    Cleans up naming and util functions

--- src/test/java/org/datadog/jmxfetch/TestRemoteAppCommon.java
+++ src/test/java/org/datadog/jmxfetch/TestRemoteAppCommon.java
@@ -19,8 +19,8 @@ import java.nio.file.Path;
 
 import java.text.MessageFormat;
 
-final class FileUtil {
-    public static String readUrlContentsToString(URL url) throws IOException {
+final class ConfigUtil {
+    private static String readUrlContentsToString(URL url) throws IOException {
         StringBuilder sb = new StringBuilder();
         InputStream inputStream = url.openStream();
         try {
@@ -34,37 +34,58 @@ final class FileUtil {
         }
         return sb.toString();
     }
-}
 
-
-public class TestIntegration {
-    protected static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger("Test Integration");
-    AppConfig appConfig = spy(AppConfig.builder().build());
-    App app;
-
-
-    /** Init JMXFetch with the given YAML configuration file. */
-    protected void initApplication(String resourceName, String host, int port)
-            throws IOException {
-
+    private static String getFormattedTemplateConfig(String resourceName, String host, int port) throws IOException{
         URL yamlURL =
                 Thread.currentThread().getContextClassLoader().getResource(resourceName);
-        String yamlContents = FileUtil.readUrlContentsToString(yamlURL);
+        String yamlContents = ConfigUtil.readUrlContentsToString(yamlURL);
         String templatedContent = MessageFormat.format(yamlContents, host, port);
 
-        Path tempDirectory = Files.createTempDirectory("temp-dir");
-        
-        String yamlName = resourceName.substring(0, resourceName.indexOf(".", 0));
+        return templatedContent;
+    }
 
+    private static Path writeConfigToTempFile(String content, String yamlName) throws IOException {
+        Path tempDirectory = Files.createTempDirectory("temp-dir");
         // Create a temporary file within the temporary directory
         Path tempFile = Files.createTempFile(tempDirectory, yamlName, ".yaml");
 
         // Write the contents of the file to the temporary file
         BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toFile()));
-        writer.write(templatedContent);
+        writer.write(content);
         writer.close();
 
-        String confdDirectory = tempDirectory.toString();
+        return tempFile;
+    }
+
+    public static Path writeTemplatedResourceToTemp(String resourceName, String host, int port) throws IOException {
+        String resourceNameWithoutExtension = resourceName.substring(0, resourceName.indexOf(".", 0));
+        String templatedContent = ConfigUtil.getFormattedTemplateConfig(resourceName, host, port);
+        Path tempFile = ConfigUtil.writeConfigToTempFile(templatedContent, resourceNameWithoutExtension);
+        return tempFile;
+    }
+}
+
+
+/* 
+ * Similar to TestCommon which assumes a local JVM connection,
+ * this provides basic app initialization for remote JMX tests
+ */
+public class TestRemoteAppCommon {
+    protected static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger("Test Integration");
+    AppConfig appConfig = spy(AppConfig.builder().build());
+    App app;
+
+    /*
+     * Init JMXFetch with the given YAML configuration template 
+     * The `resourceName` should be a full file name (with .yaml extension) in 
+     * `resources` and should utilize Java MessageFormat templating for Host
+     * and Port connection info. An example can be found in `jmxint_solr.yaml`
+    */
+    protected void initApplication(String resourceName, String host, int port)
+            throws IOException {
+        Path tempFile = ConfigUtil.writeTemplatedResourceToTemp(resourceName, host, port);
+
+        String confdDirectory = tempFile.getParent().toString();
         String yamlFileName = tempFile.getFileName().toString();
         
         List<String> params = new ArrayList<String>();
