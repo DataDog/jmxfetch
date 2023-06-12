0a454201bda14deffcf472d5e80fc0478168e810
Scott Opell <me@scottopell.com>
2023-06-12 19:22:40 +0000

    Adds HTTP check using testcontainers run

--- src/test/java/org/datadog/jmxfetch/TestContainerSanity.java
+++ src/test/java/org/datadog/jmxfetch/TestContainerSanity.java
@@ -2,6 +2,10 @@ package org.datadog.jmxfetch;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
@@ -9,14 +13,31 @@ import org.testcontainers.containers.wait.strategy.Wait;
 
 public class TestContainerSanity {
     @Rule
-    public GenericContainer<?> cont = new GenericContainer<>("testcontainers/helloworld")
-        .withExposedPorts(8080)
-        .waitingFor(Wait.forHttp("/ping").forPort(8080).forStatusCode(200));
+    public GenericContainer<?> cont = new GenericContainer<>("strm/helloworld-http")
+        .withExposedPorts(80)
+        .waitingFor(Wait.forHttp("/").forPort(80).forStatusCode(200));
 
-    
+    private static boolean isHttpOk(String host, int port) throws IOException {
+        String url = "http://" + host + ":" + port;
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(500); // fail fast, only half a second to respond
+
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
     @Test
     public void testSimple() throws Exception {
-        assertTrue(true);
+        assertTrue(isHttpOk(cont.getHost(), cont.getMappedPort(80)));
     }
 
 }
