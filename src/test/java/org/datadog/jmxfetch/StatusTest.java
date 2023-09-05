71754b217cb023271d3798dcd39f0f0c55cc1d84
raymond zhao <raymond.zhao@datadoghq.com>
2023-09-05 12:59:22 -0400

    remove unnecessary imports

--- src/test/java/org/datadog/jmxfetch/StatusTest.java
+++ src/test/java/org/datadog/jmxfetch/StatusTest.java
@@ -2,20 +2,15 @@ package org.datadog.jmxfetch;
 
 import static org.junit.Assert.*;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.datadog.jmxfetch.util.InstanceTelemetry;
-import org.datadog.jmxfetch.util.InstanceTelemetryMBean;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
