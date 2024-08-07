b95cef97bdab2508cb3027e647bc5c7b79130a1f
Carlos Roman <carlos.roman@datadoghq.com>
2024-08-07 12:38:55 +0100

    Adding unit test for IBM G9 GC metrics

--- src/test/java/org/datadog/jmxfetch/util/server/JDKImage.java
+++ src/test/java/org/datadog/jmxfetch/util/server/JDKImage.java
@@ -4,7 +4,8 @@ public enum JDKImage {
     BASE("base"),
     JDK_11("eclipse-temurin:11"),
     JDK_17("eclipse-temurin:17"),
-    JDK_21("eclipse-temurin:21");
+    JDK_21("eclipse-temurin:21"),
+    JDK_11_OPENJ9("adoptopenjdk/openjdk11-openj9:latest");
 
     private final String image;
 
