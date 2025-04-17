ecbce97c08f02536763b07bbf7d69571bea5b27f
YG <yury.gribkov@gmail.com>
2025-04-17 01:40:53 -0700

    Encapsulate JsonParser error type (#553)
    Hide the jackson exception inside the wrapper to avoid a dependency on jackson outside of this class to distinguish between IO and JSON errors. It's needed for Java Tracer, which embeds JMXFetch, excluding jackson transitive dependencies that are not needed or exercised for Java Tracer use cases.

--- src/main/java/org/datadog/jmxfetch/JsonParser.java
+++ src/main/java/org/datadog/jmxfetch/JsonParser.java
@@ -1,5 +1,6 @@
 package org.datadog.jmxfetch;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.jr.ob.JSON;
 
 import java.io.IOException;
@@ -11,10 +12,34 @@ import java.util.Map;
 @SuppressWarnings("unchecked")
 class JsonParser {
 
+    /**
+     * Hide the jackson exception inside the wrapper to avoid a dependency on jackson outside of
+     * this class to distinguish between IO and JSON errors. It's needed for Java Tracer, which
+     * embeds JMXFetch, excluding jackson transitive dependencies that are not needed or exercised
+     * for Java Tracer use cases.
+     */
+    public static final class JsonException extends IOException {
+        private final Exception exception;
+
+        public JsonException(Exception exception) {
+            super();
+            this.exception = exception;
+        }
+
+        @Override
+        public String toString() {
+            return exception.toString();
+        }
+    }
+
     private Map<String, Object> parsedJson;
 
     public JsonParser(InputStream jsonInputStream) throws IOException {
-        parsedJson = JSON.std.mapFrom(new InputStreamReader(jsonInputStream));
+        try {
+            parsedJson = JSON.std.mapFrom(new InputStreamReader(jsonInputStream));
+        } catch (JsonProcessingException ex) {
+            throw new JsonException(ex);
+        }
     }
 
     public JsonParser(JsonParser other) {
